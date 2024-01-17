d38a7b7b69105e526e25d772ff2afaa0a7a1a3b4
baumjosh <91279154+baumjosh@users.noreply.github.com>
2024-01-17 16:43:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 from flask import Flask, request, jsonify
+from flask import url_for
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'index'
 
-@app.route("/get-user/<user_id>")
-def get_user(user_id):
-    user_data = {
-        "user_id": user_id,
-        "name": "John Doe",
-        "email": "john.doe@example.com"
-    }
+@app.route('/<name>')
+def print_name(name):
+    return f'Hello, {name}' 
 
-    extra = request.args.get("extra")
-    if extra:
-        user_data["extra"] = extra
-
-    return jsonify(user_data), 200
-
-@app.route("/create-user", methods=["POST"])
-def create_user():
-    data = request.get_json()
-
-    return jsonify(data), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
