7124bea876b25094c172dec67ee5cac403325c4e
baumjosh <91279154+baumjosh@users.noreply.github.com>
2024-01-20 13:20:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ import requests
 
 
 url = "https://books.toscrape.com/"
-
-page_to_scrape = requests.get(url)
-soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-
-books = soup.find_all("aritcle", class_= "product-pod")
-price = soup.find_all("p", class_ = "price_color")
+valorant_page = "https://tracker.gg/valorant/profile/riot/Otto%230tto/matches"
 
 
+page = requests.get(valorant_page)
+soup = BeautifulSoup(page.text, "html.parser")
+
+test = soup.find("div", class_ = "trn-gamereport-list trn-gamereport-list--compact")
+print(test)
 
 
+#test = soup.find("data-v-63b27203", class_ = "trn-match-row__text-value")
+#test = soup.find_all("div", class_= "trn-match-row__text-value")
+#print(test)
+#books = soup.find_all("aritcle", class_= "product-pod")
+#price = soup.find_all("p", class_ = "price_color")
+
+
+
+
+#I think this is the path to the data element I want to extract
 '''
 > div.trn-wrapper 
 > div.trn-container 
